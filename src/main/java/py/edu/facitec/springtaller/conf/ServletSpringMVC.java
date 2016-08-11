edc4e95004a69ef4159336d13966481e8d3a60b4
Derlis López <Derlis Lpez>
2016-08-11 17:32:49 -0400

    Trabajo Práctico Nro 1 Derlid López

---
+++ src/main/java/py/edu/facitec/springtaller/conf/ServletSpringMVC.java
@@ -0,0 +1,26 @@
+package py.edu.facitec.springtaller.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer { 
+	   
+	@Override 
+	protected Class<?>[] getRootConfigClasses() {  
+		return new Class[]{AppWebConfiguration.class};
+	}
+	 
+	  
+	 
+	 @Override  
+	 protected Class<?>[] getServletConfigClasses() { 
+		 
+		 return new Class[] {};  
+	 }
+	 
+	 //ServletMappings utilizado anteriormente en el web.xml 
+	 @Override 
+	 protected String[] getServletMappings() {   
+		 return new String[] {"/"};  
+		 } 
+	 }
+	 
